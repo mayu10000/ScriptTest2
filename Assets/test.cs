2c21f06cbabc23f164d16fa8b3eb21ca4d574b85
mayu <trachan15-wan4@nifty.com>
2018-02-08 00:29:58 +0900

    Git にあげてなかったので　あげました

---
+++ Assets/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        //コンソールに　Hello,　world　と　表示
+        Debug.Log("Hello,world!");
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
